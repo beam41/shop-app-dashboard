00abafbfab90c36213e09e2f9ad101b69bd99f45
Meehoi <beam_baimon@hotmail.com>
2020-09-21 19:12:06 +0700

    add more feature to old image selector

--- components/Product/OldImageSelector.vue
+++ components/Product/OldImageSelector.vue
@@ -1,13 +1,20 @@
 <template>
-  <v-row v-if="loading">
+  <v-row v-if="loading" class="img-row flex flex-nowrap">
     <v-col v-for="i in 4" :key="i" cols="3">
-      <v-skeleton-loader class="mx-auto" max-width="300" type="card" />
+      <v-card>
+        <v-skeleton-loader class="mx-auto" max-width="300" type="card" />
+      </v-card>
     </v-col>
   </v-row>
-  <v-row v-else>
+  <v-row v-else class="img-row flex flex-nowrap">
     <v-col v-for="img in images" :key="img.id" cols="3">
       <v-card class="mx-auto" max-width="300">
-        <v-img aspect-ratio="1" :src="img.imageUrl" />
+        <v-img
+          aspect-ratio="1"
+          :src="img.imageUrl"
+          :class="saving ? 'img-saving' : null"
+          :alt="img.imageUrl"
+        />
         <v-card-actions>
           <v-checkbox
             v-model="img.markForDelete"
@@ -35,4 +42,27 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.img-row {
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    height: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #eeeeee;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #bdbdbd;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #757575;
+  }
+}
+.img-saving {
+  filter: grayscale(1);
+}
+</style>
