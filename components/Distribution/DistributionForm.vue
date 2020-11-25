6a010f2304ea4e250d5b810e4ea8c2574af48ecc
Meehoi <beam_baimon@hotmail.com>
2020-11-25 15:57:01 +0700

    payment and distribution

--- components/Distribution/DistributionForm.vue
+++ components/Distribution/DistributionForm.vue
@@ -1,55 +1,75 @@
 <template>
-  <v-form
-    ref="form"
-    class="form d-flex flex-column justify-space-between"
-    @submit.prevent="submit"
-  >
-    <div>
-      <v-row v-for="(d, index) in value" :key="index">
-        <v-col cols="6">
-          <v-text-field
-            v-model="d.name"
-            :rules="rules"
-            label="วิธีจัดส่ง"
-            required
-            :disabled="saving"
-            outlined
-            dense
-          />
-        </v-col>
-        <v-col cols="5">
-          <v-text-field
-            v-model="d.price"
-            :rules="rules"
-            label="ราคา"
-            required
-            :disabled="saving"
-            outlined
-            dense
-          />
-        </v-col>
-        <v-col cols="1" class="d-flex justify-center">
-          <v-btn
-            icon
-            color="error"
-            :loading="saving"
-            @click="$emit('delete', index)"
-          >
-            <v-icon>mdi-delete</v-icon>
+  <div class="dis-form">
+    <v-form
+      ref="form"
+      class="form d-flex flex-column justify-space-between"
+      @submit.prevent="submit"
+    >
+      <div>
+        <v-row v-for="(d, index) in value" :key="index">
+          <v-col cols="6">
+            <v-text-field
+              v-model="d.name"
+              :rules="rules"
+              label="วิธีจัดส่ง"
+              required
+              :disabled="saving"
+              outlined
+              dense
+            />
+          </v-col>
+          <v-col cols="5">
+            <v-text-field
+              v-model="d.price"
+              :rules="rules"
+              label="ราคา"
+              required
+              :disabled="saving"
+              outlined
+              dense
+            />
+          </v-col>
+          <v-col cols="1" class="d-flex justify-center">
+            <v-btn
+              icon
+              color="error"
+              :loading="saving"
+              @click="$emit('delete', index)"
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-btn block color="accent" outlined @click="$emit('add')">
+          <v-icon>mdi-plus</v-icon> เพิ่มแถว
+        </v-btn>
+      </div>
+      <div class="d-flex pt-16">
+        <v-spacer />
+        <v-btn large color="primary" :loading="saving" type="submit">
+          บันทึก
+        </v-btn>
+      </div>
+    </v-form>
+    <!-- add edit dialog -->
+    <v-dialog v-model="saveDialog" max-width="400">
+      <v-card>
+        <v-card-title class="headline">บันทึกข้อมูล?</v-card-title>
+
+        <v-card-text> ต้องการบันทึกข้อมูลวิธีการจัดส่งหรือไม่ </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text :disabled="saving" @click="saveDialog = false">
+            ยกเลิก
           </v-btn>
-        </v-col>
-      </v-row>
-      <v-btn block color="accent" outlined @click="$emit('add')">
-        <v-icon>mdi-plus</v-icon> เพิ่มแถว
-      </v-btn>
-    </div>
-    <div class="d-flex pt-16">
-      <v-spacer />
-      <v-btn large color="primary" :loading="saving" type="submit">
-        บันทึก
-      </v-btn>
-    </div>
-  </v-form>
+          <v-btn color="primary" text :loading="saving" @click="save">
+            บันทึก
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -63,12 +83,17 @@ export default {
   },
   data: () => ({
     rules: [(v) => !!v || 'โปรดกรอกข้อมูลให้ครบถ้วน'],
+    saveDialog: false,
   }),
   methods: {
     submit() {
       if (this.$refs.form.validate()) {
-        this.$emit('submit')
+        this.saveDialog = true
       }
+    },
+    save() {
+      this.$emit('submit')
+      this.saveDialog = false
     },
   },
 }
@@ -76,6 +101,10 @@ export default {
 
 <style lang="scss" scoped>
 .form {
+  width: 100%;
+}
+
+.dis-form {
   width: 100%;
   max-width: 500px;
 }
--- components/Payment/PaymentForm.vue
+++ components/Payment/PaymentForm.vue
@@ -1,66 +1,88 @@
 <template>
-  <v-form
-    ref="form"
-    class="form d-flex flex-column justify-space-between"
-    @submit.prevent="submit"
-  >
-    <div>
-      <v-row v-for="(d, index) in value" :key="index">
-        <v-col cols="3">
-          <v-text-field
-            v-model="d.bank"
-            :rules="rules"
-            label="ชื่อธนาคาร"
-            required
-            :disabled="saving"
-            outlined
-            dense
-          />
-        </v-col>
-        <v-col cols="4">
-          <v-text-field
-            v-model="d.accountNumber"
-            :rules="rules"
-            label="เลขที่บัญชี"
-            required
-            :disabled="saving"
-            outlined
-            dense
-          />
-        </v-col>
-        <v-col cols="4">
-          <v-text-field
-            v-model="d.accountName"
-            :rules="rules"
-            label="ชื่อบัญชี"
-            required
-            :disabled="saving"
-            outlined
-            dense
-          />
-        </v-col>
-        <v-col cols="1" class="d-flex justify-center">
-          <v-btn
-            icon
-            color="error"
-            :loading="saving"
-            @click="$emit('delete', index)"
-          >
-            <v-icon>mdi-delete</v-icon>
+  <div class="pm-form">
+    <v-form
+      ref="form"
+      class="form d-flex flex-column justify-space-between"
+      @submit.prevent="submit"
+    >
+      <div>
+        <v-row v-for="(d, index) in value" :key="index">
+          <v-col cols="3">
+            <v-text-field
+              v-model="d.bank"
+              :rules="rules"
+              label="ชื่อธนาคาร"
+              required
+              :disabled="saving"
+              outlined
+              dense
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="d.accountNumber"
+              :rules="rules"
+              label="เลขที่บัญชี"
+              required
+              :disabled="saving"
+              outlined
+              dense
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="d.accountName"
+              :rules="rules"
+              label="ชื่อบัญชี"
+              required
+              :disabled="saving"
+              outlined
+              dense
+            />
+          </v-col>
+          <v-col cols="1" class="d-flex justify-center">
+            <v-btn
+              icon
+              color="error"
+              :loading="saving"
+              @click="$emit('delete', index)"
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-btn block color="accent" outlined @click="$emit('add')">
+          <v-icon>mdi-plus</v-icon> เพิ่มแถว
+        </v-btn>
+      </div>
+      <div class="d-flex pt-16">
+        <v-spacer />
+        <v-btn large color="primary" :loading="saving" type="submit">
+          บันทึก
+        </v-btn>
+      </div>
+    </v-form>
+    <!-- add edit dialog -->
+    <v-dialog v-model="saveDialog" max-width="400">
+      <v-card>
+        <v-card-title class="headline">บันทึกข้อมูล?</v-card-title>
+
+        <v-card-text>
+          ต้องการบันทึกข้อมูลช่องทางการชำระเงินหรือไม่
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text :disabled="saving" @click="saveDialog = false">
+            ยกเลิก
           </v-btn>
-        </v-col>
-      </v-row>
-      <v-btn block color="accent" outlined @click="$emit('add')">
-        <v-icon>mdi-plus</v-icon> เพิ่มแถว
-      </v-btn>
-    </div>
-    <div class="d-flex pt-16">
-      <v-spacer />
-      <v-btn large color="primary" :loading="saving" type="submit">
-        บันทึก
-      </v-btn>
-    </div>
-  </v-form>
+          <v-btn color="primary" text :loading="saving" @click="save">
+            บันทึก
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -74,12 +96,17 @@ export default {
   },
   data: () => ({
     rules: [(v) => !!v || 'โปรดกรอกข้อมูลให้ครบถ้วน'],
+    saveDialog: false,
   }),
   methods: {
     submit() {
       if (this.$refs.form.validate()) {
-        this.$emit('submit')
+        this.saveDialog = true
       }
+    },
+    save() {
+      this.$emit('submit')
+      this.saveDialog = false
     },
   },
 }
@@ -87,6 +114,10 @@ export default {
 
 <style lang="scss" scoped>
 .form {
+  width: 100%;
+}
+
+.pm-form {
   width: 100%;
   max-width: 1000px;
 }
